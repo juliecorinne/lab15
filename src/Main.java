3d0d8b71a6632aa66116693ac302adc10dcbd207
Julie Schneider <julieschneider@Julies-MacBook-Pro.local>
2017-02-13 16:34:59 -0500

    lab15

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        String country = " ";
         int input = 0;
-        String userInput = " ";
 
         System.out.println("Welcome to this super useful Country App!");
 
@@ -18,38 +16,61 @@ public class Main {
             input = CountriesApp.rules();
             System.out.println();
 
-            if (input == 1) {
-
-                CountriesApp.userPickOne(input);
-
-                System.out.println("Would you like to try another command? y/n");
-                userInput = scan.nextLine();
-
-                if (userInput.equalsIgnoreCase("y")) {
-                    countryList = true;
-                } else {
-                    countryList = false;
-                }
-
-            } else if (input == 2) {
-
-                CountriesApp.userPickTwo(input);
-
-                System.out.println("Would you like to try another command? y/n");
-                userInput = scan.nextLine();
-
-                if (userInput.equalsIgnoreCase("y")) {
-                    countryList = true;
-                } else {
-                    countryList = false;
-                }
-
-            } else {
-                countryList = false;
-            }
+            countryList = ifElseForUser(scan, input);
         }
 
         CountriesApp.userPickThree(input);
 
+    }
+
+    public static boolean ifElseForUser(Scanner scan, int input) {
+
+        boolean countryList;
+        if (input == 1) {
+
+            countryList = pickOne(scan, input);
+
+        } else if (input == 2) {
+
+            countryList = pickTwo(scan, input);
+
+        } else {
+            countryList = false;
+        }
+        return countryList;
+    }
+
+    public static boolean pickTwo(Scanner scan, int input) {
+
+        String userInput;
+        boolean countryList;
+        CountriesApp.userPickTwo(input);
+
+        System.out.println("Would you like to try another command? y/n");
+        userInput = scan.nextLine();
+
+        if (userInput.equalsIgnoreCase("y")) {
+            countryList = true;
+        } else {
+            countryList = false;
+        }
+        return countryList;
+    }
+
+    public static boolean pickOne(Scanner scan, int input) {
+
+        String userInput;
+        boolean countryList;
+        CountriesApp.userPickOne(input);
+
+        System.out.println("Would you like to try another command? y/n");
+        userInput = scan.nextLine();
+
+        if (userInput.equalsIgnoreCase("y")) {
+            countryList = true;
+        } else {
+            countryList = false;
+        }
+        return countryList;
     }
 }
